17c56996da4ca95c3d7507ee8bbb5d7fb2e14766
kenzonawa <vckznw@gmail.com>
2017-04-20 15:48:37 -0400

    v2
    going

---
+++ portfolio/js/javascript.js
@@ -0,0 +1,11 @@
+jQuery(document).on('click', 'a', function(){
+	var fades = document.getElementById("body");
+	fades.className += " fadeOut";
+
+});
+
+function fadeOut(){
+	var fades = document.getElementById("body");
+	fades.className += " fadeOut";
+
+}
---
+++ portfolio/js/portfolio.js
@@ -0,0 +1,15 @@
+// CHANGE NAV BAR COLOR
+/*$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav').css('background-color', 'rgba(34,34,34,1.0)');
+       } else {
+          $('.nav').css('background-color', 'white');
+       }
+   });
+});*/
+
