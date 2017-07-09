cb6dad893851d7ce1404d3e746a0ba1249f21479
kenzonawa <vckznw@gmail.com>
2017-07-09 12:08:08 -0600

    functions

--- css/javascript.js
+++ css/javascript.js
@@ -5,23 +5,22 @@ console.log("loaded");
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    var block1 = $("#block1").offset().top-300;
-    var block2 = $("#block2").offset().top-300;
-    var block3 = $("#block3").offset().top-300;
+    var block1 = $("#block1").offset().top-100;
+    var block2 = $("#block2").offset().top-100;
+    var block3 = $("#block3").offset().top-100;
 
-    if(scroll >= block1) {
-        $("#step1").addClass("active");
-    }
+    showUp(block1,"#step1");
+    showUp(block2,"#step2");
+    showUp(block3,"#step3");
 
-    if(scroll <= block1) {
-        $("#step1").removeClass("active");
-    } 
-
-    if(scroll >= block2) {
-        $("#step2").addClass("active");
-    }
-
-    if(scroll <= block2) {
-        $("#step2").removeClass("active");
-    } 
 });
+
+function showUp(block,id){
+    if(scroll >= block) {
+        $(id).addClass("active");
+    }
+
+    if(scroll <= block) {
+        $(id).removeClass("active");
+    } 
+}
