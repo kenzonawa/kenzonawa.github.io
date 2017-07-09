4081caa867c202c3661bda77a5f66a18e144ea2c
kenzonawa <vckznw@gmail.com>
2017-07-09 12:04:01 -0600

    css

--- css/javascript.js
+++ css/javascript.js
@@ -5,22 +5,23 @@ console.log("loaded");
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    var block1 = $("#block1").offset().top;
-    var block2 = $("#block2").offset().top;
+    var block1 = $("#block1").offset().top-300;
+    var block2 = $("#block2").offset().top-300;
+    var block3 = $("#block3").offset().top-300;
 
     if(scroll >= block1) {
-        $("#step2").addClass("active");
+        $("#step1").addClass("active");
     }
 
     if(scroll <= block1) {
-        $("#step2").removeClass("active");
+        $("#step1").removeClass("active");
     } 
 
     if(scroll >= block2) {
-        $("#step3").addClass("active");
+        $("#step2").addClass("active");
     }
 
     if(scroll <= block2) {
-        $("#step3").removeClass("active");
+        $("#step2").removeClass("active");
     } 
 });
