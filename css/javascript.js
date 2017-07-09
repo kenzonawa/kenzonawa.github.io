8bce2af6c8d5dd101b4241ee8f00155b8223fe8f
kenzonawa <vckznw@gmail.com>
2017-07-09 11:43:01 -0600

    ds

--- css/javascript.js
+++ css/javascript.js
@@ -3,11 +3,11 @@ console.log("loaded");
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if(scroll >= 500) {
-        $("#step2").addClass("active");
+    if(scroll >= 1000) {
+        $("#step3").addClass("active");
     }
 
-    if(scroll <= 500) {
-        $("#step2").removeClass("active");
+    if(scroll <= 1000) {
+        $("#step3").removeClass("active");
     } 
 });
